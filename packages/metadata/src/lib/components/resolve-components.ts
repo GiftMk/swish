20481656cf67e4bae53350ff97c7d677b9fe0783
Gift Mkwara <gift.mkwara@gmail.com>
2025-04-13 17:41:49 +1200

    fixed minor type errors

--- packages/metadata/src/lib/components/resolve-components.ts
+++ packages/metadata/src/lib/components/resolve-components.ts
@@ -1,6 +1,6 @@
-import type { ComponentMeta } from "../../types";
+import type { ClassMeta } from "../../types";
 import type { ClassLookup } from "../class-lookup";
 
-export const resolveComponents = (lookup: ClassLookup): ComponentMeta[] => {
+export const resolveComponents = (lookup: ClassLookup): ClassMeta[] => {
   return lookup.components.map(({ declaration, ...props }) => props);
 };
